537c52a6bc5f5b5a9072a69e19dd2ee3f279f4bf
rodBarrs <rodrigosbarros51@gmail.com>
2025-04-09 19:12:58 -0300

    chore: iniciando repositorio

---
+++ src/main/java/com/rodbarrs/Golytics/GolyticsApplication.java
@@ -0,0 +1,13 @@
+package com.rodbarrs.Golytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodbarrs/Golytics/GolyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodbarrs.Golytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
